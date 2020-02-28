985a4a3f282caadca76ad3387e1cb92f79aaa0a2
Santhosh Krishnan <imskrishnan@gmail.com>
2020-02-28 14:23:14 -0500

    Added some comments to the HelloWorld.java file
    Some comments to describe the sequence of events..

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -4,13 +4,23 @@
 public class Hello {
     public static void main (String[] args) {
         System.out.println("Hello, Santhosh!");
-
+        //this is a comment And this is the firstNumber variable to show the addition of expressions
         int myFirstNumber = (10 + 5) + (2 * 10);
+        
+        //this is the secondNumber variable
         int mySecondNumber = 12;
+        
+        //this is the thirdNumber variable
         int myThirdNumber = myFirstNumber * 2;
+        
+        //this is the myTotal
         int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
         System.out.println(myTotal);
+        
+        //myLastOne var stores the result
         int myLastOne = 1000 - myTotal;
+        
+        //this is the statement to print result to the console
         System.out.println(myLastOne);
     }
 }
